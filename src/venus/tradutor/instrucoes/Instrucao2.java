747e5d73e35e0cb63b50ef6591f417d7f55b64da
Nechelley <Nechelley@live.com>
2017-03-28 00:03:01 -0300

    Add instrucoes e classe Arquivo

---
+++ src/venus/tradutor/instrucoes/Instrucao2.java
@@ -0,0 +1,74 @@
+package venus.tradutor.instrucoes;
+
+/**
+ *
+ * @author neche
+ */
+public class Instrucao2 {
+    private short func;
+    private short rc;
+    private short off;
+    
+    public Instrucao2(short func,short rc,short off){
+        this.func = func;
+        this.rc = rc;
+        this.off = off;
+    }
+    
+    /**
+     * Retorna a instrucao em forma de binario
+     * @return String da instrucao pronta para ser escrita
+     */
+    public String getBin(){
+        int conta = (short)((func << 14) | (rc << 11) | off);
+        String retorno = Integer.toBinaryString(conta);
+        String aux = "";
+        for(int i = 0;i < 16 - retorno.length();i++){
+            aux += "0";
+        }
+        return (aux + retorno);
+    }
+    
+    /**
+     * @return the func
+     */
+    public short getFunc() {
+        return func;
+    }
+
+    /**
+     * @param func the func to set
+     */
+    public void setFunc(short func) {
+        this.func = func;
+    }
+
+    /**
+     * @return the rc
+     */
+    public short getRc() {
+        return rc;
+    }
+
+    /**
+     * @param rc the rc to set
+     */
+    public void setRc(short rc) {
+        this.rc = rc;
+    }
+
+    /**
+     * @return the off
+     */
+    public short getOff() {
+        return off;
+    }
+
+    /**
+     * @param off the off to set
+     */
+    public void setOff(short off) {
+        this.off = off;
+    }
+    
+}
---
+++ src/venus/tradutor/instrucoes/Instrucao5.java
@@ -0,0 +1,74 @@
+package venus.tradutor.instrucoes;
+
+/**
+ *
+ * @author neche
+ */
+public class Instrucao5 {
+    private short func;
+    private short op;
+    private short off;
+    
+    public Instrucao5(short func,short op,short off){
+        this.func = func;
+        this.op = op;
+        this.off = off;
+    }
+    
+    /**
+     * Retorna a instrucao em forma de binario
+     * @return String da instrucao pronta para ser escrita
+     */
+    public String getBin(){
+        int conta = (short)((func << 14) | (op << 12) | off);
+        String retorno = Integer.toBinaryString(conta);
+        String aux = "";
+        for(int i = 0;i < 16 - retorno.length();i++){
+            aux += "0";
+        }
+        return (aux + retorno);
+    }
+
+    /**
+     * @return the func
+     */
+    public short getFunc() {
+        return func;
+    }
+
+    /**
+     * @param func the func to set
+     */
+    public void setFunc(short func) {
+        this.func = func;
+    }
+
+    /**
+     * @return the op
+     */
+    public short getOp() {
+        return op;
+    }
+
+    /**
+     * @param op the op to set
+     */
+    public void setOp(short op) {
+        this.op = op;
+    }
+
+    /**
+     * @return the off
+     */
+    public short getOff() {
+        return off;
+    }
+
+    /**
+     * @param off the off to set
+     */
+    public void setOff(short off) {
+        this.off = off;
+    }
+    
+}
