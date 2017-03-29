d70a03750555dac4143a1ffa6c9b991be18ca3d3
Nechelley <Nechelley@live.com>
2017-03-28 21:49:59 -0300

    Add MEM

---
+++ src/venus/cpu/MEM.java
@@ -0,0 +1,48 @@
+package venus.cpu;
+
+import cpu.memoria.MemoriaPrincipal;
+
+/**
+ *
+ * @author neche
+ */
+public class MEM {
+    private static MEM instance = null;
+    private MemoriaPrincipal banco;
+    private int endereco;
+    private short readData;
+    
+    private MEM(){
+        banco = MemoriaPrincipal.getInstance();
+        endereco = 0;
+        readData = 0;
+    };
+    
+    public static MEM getInstance(){
+        if(instance == null)
+            instance = new MEM();
+        return instance;
+    }
+    
+    public void clock(){
+        //pega o controle
+        Controle c = Controle.getInstance();
+        IF pif = IF.getInstance();
+        ULA ula = ULA.getInstance();
+        ID id = ID.getInstance();
+        
+        if(c.getMemRead()){//se tiver que ler
+            endereco = ula.getResult();//onde deve ler
+            
+            readData = banco.get(endereco);
+        }else if(c.getMemWrite()){//se tiver que escrever
+            endereco = ula.getResult();//onde deve escrever
+            
+            banco.inserir(endereco, (short) id.getReadData2());
+        }
+    }
+    
+    public short getReadData(){
+        return readData;
+    }
+}
