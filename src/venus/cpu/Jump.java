cd18176b8f6229c9c1cb723142e1311c1a4a09c8
Diego Sousa <diegosousa.st@gmail.com>
2017-03-29 00:55:08 -0300

    Jump Completo

--- src/venus/cpu/Jump.java
+++ src/venus/cpu/Jump.java
@@ -8,9 +8,10 @@ package venus.cpu;
 import venus.cpu.memoria.MemoriaRegistrador;
 import venus.cpu.unidades.IF;
 import venus.cpu.unidades.unidadesComplementares.ExtensorSinal;
+import venus.cpu.controller.ULA;
 
 /**
- * Classe que resolve os jumps incon
+ * Classe que resolve todos os jumps
  * @author Diego
  */
 public class Jump {    
@@ -35,10 +36,41 @@ public class Jump {
         bancoDeRegistradores = MemoriaRegistrador.getInstance();
     }
     /**
-     * Método que é executado toda vez que é
+     * Método que é executado todo clock para testar se há um desvio ou nao
      */
     public void clock(){
         checkJump();
+    }
+    /**
+     * Método que verifica a flag na ula dependendo do cond da instrução de desvio condicional
+     * @return Boolean representando o resultado da flag cond
+     */
+    private boolean getFlag(){
+        short cond = If.getSubBin(4, 4);
+        boolean aux = false;
+        switch(cond){            
+            case(0b0100):
+                aux = ula.getNeg();
+                break;
+            case(0b0101):
+                aux = ula.getZero();
+                break;
+            case(0b0110):
+                aux = ula.getCarry();
+                break;
+            case(0b0111):
+                aux = ula.getNegzero();
+                break;
+            case(0b0000):
+                aux = ula.getTrue();
+                break;
+            case(0b0011):
+                aux = ula.getOverflow();
+                break;
+            default:
+                throw new RuntimeException("COND nao esta no formato especificado");
+        }
+        return aux;
     }
     /**
      * Método que verifica a existencia de um jump e desvia caso necessário
@@ -47,13 +79,17 @@ public class Jump {
         if(unidadeDeControle.getJump()){
             ExtensorSinal extensor = ExtensorSinal.getInstance();
             if(unidadeDeControle.getOp() == 0b0){
-                if(ula.){
-                    //desvio relativo
-                }
+                //desvio jf cond destino
+                if(!getFlag()){
+                    short desvio = extensor.exetender8();
+                    If.setPcRelativo(desvio);
+                }                
             }
             else if(unidadeDeControle.getOp() == 0b01){
-                if(ula.){
-                    //desvio relativo    
+                //desvio jt cond destino
+                if(getFlag()){
+                    short desvio = extensor.exetender8();
+                    If.setPcRelativo(desvio);
                 }                    
             }
             else if(unidadeDeControle.getOp() == 0b10){
@@ -61,6 +97,7 @@ public class Jump {
                 If.setPcRelativo(desvio);
             }
             else if(unidadeDeControle.getR() == 0b0){
+                //jal B
                 short reg = 7;
                 bancoDeRegistradores.inserir(reg,If.getPc());
                 //pega o valor do registrador B e seto ele como o desvio
@@ -68,6 +105,7 @@ public class Jump {
                 If.setPcPseudoDireto(desvio);                
             }
             else{
+                //jr reg
                 short desvio = bancoDeRegistradores.get(If.getSubBin(13, 3));
                 If.setPcPseudoDireto(desvio);                
             }                            
